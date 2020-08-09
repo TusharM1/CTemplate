60b9b39a6d864688b38ff99102ff16bb761fc07a
Tushar Mahajan <tusharmahajan@Tushars-MacBook-Pro.local>
2020-08-09 16:33:33 -0400

    Doxygen

---
+++ README.md
@@ -0,0 +1,3 @@
+# This is the README file
+
+This is the project.
--- src/client/client.c
+++ src/client/client.c
@@ -3,8 +3,21 @@
 
 #include "commands.h"
 
+/**
+ * @file client.c
+ * @author Tushar Mahajan
+ * @date 9 Aug 2020
+ * @brief This is the main file for the client.
+ * This file contains all the code exclusive to the client.
+ */
+
+/**
+ * @brief This is the main function that runs the client.
+ * @param argc Command line arguments count
+ * @param argv Command line arguments
+ * @return int Program status return code
+ */
 int main(int argc, char* argv[]) {
-	int command = commands();
-	printf("Client %d\n", command);
+	printf("Client %d\n", commands());
 	return EXIT_SUCCESS;
 }
