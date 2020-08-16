cdb4ccf60bfd4701d686d835d7706eaae556a986
Tushar Mahajan <tman08810@gmail.com>
2020-08-15 21:12:23 -0400

    added unity testing framework

--- cmake
+++
@@ -1,6 +0,0 @@
-# Create build directory for cmake output
-mkdir -r build build/x64-Debug build/x64-Release
-
-# Initialize CMake for 64 bit release and debug modes
-cmake -B build/x64-Debug -DCMAKE_BUILD_TYPE=Debug
-cmake -B build/x64-Release -DCMAKE_BUILD_TYPE=Release
--- tests/tester.c
+++ tests/tester.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
-int main() {
-	printf("This is the tester\n");
-	return 0;
+#include <unity.h>
+
+#include "commands.h"
+
+void setUp(){}
+void tearDown(){}
+
+void test_commands() {
+	TEST_ASSERT_EQUAL(2, commands());
+}
+
+int main(void) {
+	UNITY_BEGIN();
+
+	RUN_TEST(test_commands);
+
+	return UNITY_END();
 }
